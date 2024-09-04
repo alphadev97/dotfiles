b1ae6aa61c75f4676e9b1ef7bfcc3019dd29c9b1
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-09-04 10:30:52 +0500

    Tranparency

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,18 +7,18 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = "100"
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+	vim.opt.clipboard = "unnamedplus"
 end)
 vim.opt.breakindent = true
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.termguicolors = true
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,19 @@ return {
   lazy = false,
   priority = 9999,
   config = function()
+    -- Set the background transparency
+    vim.cmd([[
+      augroup TransparentBG
+        autocmd!
+        autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
+        autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
+        autocmd ColorScheme * highlight LineNr ctermbg=none guibg=none
+        autocmd ColorScheme * highlight Folded ctermbg=none guibg=none
+        autocmd ColorScheme * highlight EndOfBuffer ctermbg=none guibg=none
+      augroup END
+    ]])
+
+    -- Set the colorscheme
     vim.cmd("colorscheme gruvbox")
   end
 }
