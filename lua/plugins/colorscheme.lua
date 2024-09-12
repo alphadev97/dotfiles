554de725d55a0f99805fbf0bda008df5b8d9e4a6
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-09-12 21:29:29 +0500

    change color theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			transparent_background = true,
-			term_colors = true,
-			integrations = {
-				nvimtree = true,
-				telescope = true,
-				lsp_saga = true,
-			},
-		})
-		vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({
+      transparent_mode = true, -- Enable transparent background
+      contrast = "hard", -- Options: "hard", "medium", "soft" (adjust to your liking)
+      overrides = {
+        Normal = { bg = "none" }, -- Removes background color for transparency
+        NormalFloat = { bg = "none" }, -- For floating windows
+      },
+    })
+
+    -- Set the color scheme to gruvbox
+    vim.cmd.colorscheme("gruvbox")
+  end,
 }
