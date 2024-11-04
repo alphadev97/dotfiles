9ec9817bc199d1775a85f3ce8ac8c6719a590c3e
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-11-04 16:05:16 +0500

    Updated

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,26 +1,12 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        require("neo-tree").setup({
-            filesystem = {
-                filtered_items = {
-                    visible = true,  -- Show hidden files (dotfiles)
-                    hide_dotfiles = false,  -- Do not hide dotfiles
-                    hide_by_name = {  -- Add files or folders you want to hide
-                        "node_modules",
-                    },
-                },
-            },
-        })
-
-        vim.keymap.set("n", "\\", ":Neotree toggle<CR>", {})
-        vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-    end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", 
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')
+  end
 }
-
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+  'nvim-treesitter/nvim-treesitter', 
+  build = ':TSUpdate',
   config = function()
-    local config = require("nvim-treesitter.configs")
+    local config = require('nvim-treesitter.configs')
     config.setup({
-      ensure_installed = { "lua", "javascript", "go" },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+      ensure_installed = {"lua", "javascript", "go"},
+      highlight = {enable = true},
+      indent = {enable = false},
     })
-  end,
+  end
 }
