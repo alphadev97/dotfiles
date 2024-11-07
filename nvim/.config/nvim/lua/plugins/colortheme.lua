d182a18b49690abcff5af1de2c39294371d28aba
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-11-07 18:43:54 +0500

    colortheme

--- nvim/.config/nvim/lua/plugins/colortheme.lua
+++ nvim/.config/nvim/lua/plugins/colortheme.lua
@@ -6,3 +6,12 @@ return {
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
+
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd("colorscheme rose-pine-main")
+-- 	end,
+-- }
