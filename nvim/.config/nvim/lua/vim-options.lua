c9e618755e4958f43d509b9cb56044ad847fbb9d
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-11-11 15:55:58 +0500

    Modularize

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,17 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.g.maplocalleader = ' '
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
