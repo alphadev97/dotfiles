09ede764a47050a8c1494c853c5d5cb8515eaf76
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-11-15 19:39:18 +0500

    bufferline, noice

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,22 @@ return {
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    require("bufferline").setup({})
+    require("bufferline").setup({
+      options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true,
+          },
+        },
+      },
+    })
   end,
 }
