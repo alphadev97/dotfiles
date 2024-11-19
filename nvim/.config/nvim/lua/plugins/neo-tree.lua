3b8f2528eb7f974c5048bd46900008a845ae1109
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-11-19 20:49:26 +0500

    Neotree - hidden files/folders

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
-return   {
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
@@ -7,6 +7,19 @@ return   {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<leader>e', ":Neotree toggle<CR>")
-  end
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules",
+          },
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+  end,
 }
