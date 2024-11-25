15470d47a6ca8e16b4b814a24cb391e45a68d208
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-11-26 01:19:25 +0500

    colortheme, none-ls

--- nvim/.config/nvim/lua/plugins/colortheme.lua
+++ nvim/.config/nvim/lua/plugins/colortheme.lua
@@ -12,5 +12,23 @@ return {
     })
 
     vim.cmd("colorscheme rose-pine")
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+    vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none" })
+
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })        -- Main Telescope popup
+    vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })        -- Border around Telescope
+    vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })  -- Prompt window
+    vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })  -- Border around prompt window
+    vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" }) -- Results window
+    vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" }) -- Border around results window
+    vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" }) -- Preview window
+    vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" }) -- Border around preview window
   end,
 }
