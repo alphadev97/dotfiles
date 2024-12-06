d42d3f225a459cd28c73398ef93d006c289b4fc8
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-12-06 12:31:30 +0500

    Wallpapers

--- nvim/.config/nvim/lua/plugins/colortheme.lua
+++ nvim/.config/nvim/lua/plugins/colortheme.lua
@@ -3,12 +3,12 @@ return {
   priority = 1000,
   config = function()
     require("rose-pine").setup({
-      palette = {
-        main = {
-          pine = "#d3f8a4",
-          base = "#202021",
-        },
-      },
+      -- palette = {
+      --   main = {
+      --     pine = "#d3f8a4",
+      --     base = "#202021",
+      --   },
+      -- },
     })
 
     vim.cmd("colorscheme rose-pine")
