10513ebc4181643f102274448bc1ed5de4029519
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2025-01-04 19:03:52 +0500

    Bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,47 +1,54 @@
 return {
-  "akinsho/bufferline.nvim",
-  event = "VeryLazy",
-  version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("bufferline").setup({
-      options = {
-        diagnostics = "nvim_lsp",
-        diagnostics_indicator = function(count, level)
-          local icon = level:match("error") and " " or " "
-          return " " .. icon .. count
-        end,
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "File Explorer",
-            text_align = "left",
-            separator = true,
-          },
-        },
-      },
-    })
+	"akinsho/bufferline.nvim",
+	event = "VeryLazy",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						text_align = "left",
+						separator = true,
+					},
+				},
+			},
+		})
 
-    -- Keymaps for interacting with buffers using 'b' prefix
-    local keymap = vim.keymap
+		-- Keymaps for interacting with buffers using 'b' prefix
+		local keymap = vim.keymap
 
-    keymap.set("n", "bn", ":BufferLineCycleNext<CR>", { desc = "Switch to next buffer" })
+		keymap.set("n", "bn", ":BufferLineCycleNext<CR>", { desc = "Switch to next buffer" })
 
-    keymap.set("n", "bp", ":BufferLineCyclePrev<CR>", { desc = "Switch to previous buffer" })
+		keymap.set("n", "bp", ":BufferLineCyclePrev<CR>", { desc = "Switch to previous buffer" })
 
-    -- Go to buffer 1 to 9
-    for i = 1, 9 do
-      keymap.set("n", "b" .. i, ":BufferLineGoToBuffer " .. i .. "<CR>", { desc = "Switch to buffer " })
-    end
+		-- Go to buffer 1 to 9
+		for i = 1, 9 do
+			keymap.set("n", "b" .. i, ":BufferLineGoToBuffer " .. i .. "<CR>", { desc = "Switch to buffer " })
+		end
 
-    keymap.set("n", "bb", ":bd<CR>", { desc = "Close the current buffer" })
+		keymap.set("n", "bb", ":bd<CR>", { desc = "Close the current buffer" })
 
-    keymap.set("n", "bd", ":BufferLinePickClose<CR>", { desc = "Close all buffers" })
+		keymap.set("n", "bd", ":BufferLinePickClose<CR>", { desc = "Close all buffers" })
 
-    keymap.set("n", "bp", ":BufferLinePick<CR>", { desc = "Pick a buffer to switch to" })
+		keymap.set("n", "bp", ":BufferLinePick<CR>", { desc = "Pick a buffer to switch to" })
 
-    keymap.set("n", "br", ":BufferLineMoveNext<CR>", { desc = "Re-order buffers right" })
+		keymap.set("n", "br", ":BufferLineMoveNext<CR>", { desc = "Re-order buffers right" })
 
-    keymap.set("n", "bl", ":BufferLineMovePrev<CR>", { desc = "Re-order buffers left" })
-  end,
+		keymap.set("n", "bl", ":BufferLineMovePrev<CR>", { desc = "Re-order buffers left" })
+
+		keymap.set(
+			"n",
+			"bD",
+			":BufferLineCloseLeft<CR>:BufferLineCloseRight<CR>",
+			{ desc = "Close all buffers except the current one" }
+		)
+	end,
 }
