44c9bdd4e43d46853430e985bf99b523f45d48d4
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2025-03-21 02:48:24 +0500

    Save local changes before pulling

--- nvim/.config/nvim/lua/plugins/colortheme.lua
+++ nvim/.config/nvim/lua/plugins/colortheme.lua
@@ -1,30 +1,3 @@
--- return {
---   "EdenEast/nightfox.nvim",
---   priority = 1000,
---   config = function()
---     require("nightfox").setup({})
---     vim.cmd("colorscheme nordfox")
---
---     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
---     vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
---     vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
---     vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none" })
---
---     vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
---     vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
---   end,
--- }
-
 return {
   "folke/tokyonight.nvim",
   lazy = false,
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,41 +1,30 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	"nvimtools/none-ls.nvim",
+  dependencies = {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        -- Python formatters
-        null_ls.builtins.formatting.black, -- Black (recommended)
-        null_ls.builtins.formatting.isort, -- Sort imports
-        -- null_ls.builtins.formatting.autopep8,  -- Alternative (not needed if using black)
-
-        -- Other formatters
-        null_ls.builtins.formatting.stylua,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.formatting.sql_formatter,
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          -- Clear any existing autocmds to avoid duplicates
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			},
+		})
 
-          -- Create an autocmd to format on save
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false, bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 
-    -- Manual format keybinding for fallback or convenience
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format file" })
-  end,
+
+    require("mason-tool-installer").setup({
+				ensure_installed = {
+					"stylua",
+					"prettierd",
+					"goimports",
+					"golangci-lint",
+				},
+			})
+	end,
 }
