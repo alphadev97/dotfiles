bd2a183913c4565e966e059564eefdde84efdd02
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2025-03-26 21:06:40 +0500

    Neotree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,8 +6,37 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  lazy = false, 
+
   config = function()
-    vim.keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", {})
-  end
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules",
+          },
+        },
+      },
+      window = {
+        position = "current",
+      },
+      icon = {
+        folder_closed = "",
+        folder_open = "",
+        folder_empty = "󰜌",
+      },
+    })
+
+    -- TODO: hi this is todo
+
+    local keymap = vim.keymap
+
+    -- Keymaps for Neo-tree
+    keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", { desc = "Toggle file explorer" })
+    keymap.set("n", "<leader>ef", ":Neotree reveal<CR>", { desc = "Toggle file explorer on current file" })
+    keymap.set("n", "<leader>ec", ":Neotree close<CR>", { desc = "Collapse file explorer" })
+    keymap.set("n", "<leader>er", ":Neotree refresh<CR>", { desc = "Refresh file explorer" })
+  end,
 }
