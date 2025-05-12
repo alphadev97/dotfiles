4cb469a01f67824cfacb1e5c6616cad7ac8a7119
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2025-05-12 23:06:22 +0500

    Snaks

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,88 +1,86 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"gopls",
-					"ts_ls",
-					"html",
-					"cssls",
-					"tailwindcss",
-					"jsonls",
-					"emmet_ls",
-					"pyright",
-					"ruff",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    version = "^1.0.0",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    version = "^1.0.0",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "gopls",
+          "ts_ls",
+          "html",
+          "cssls",
+          "tailwindcss",
+          "jsonls",
+          "emmet_ls",
+          "pyright",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.emmet_ls.setup({
-				capabilities = capabilities,
-				filetypes = {
-					"css",
-					"eruby",
-					"html",
-					"javascript",
-					"javascriptreact",
-					"typescript",
-					"typescriptreact",
-					"less",
-					"sass",
-					"scss",
-					"svelte",
-					"pug",
-					"vue",
-				},
-			})
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = {
+          "css",
+          "eruby",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "less",
+          "sass",
+          "scss",
+          "svelte",
+          "pug",
+          "vue",
+        },
+      })
 
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-				filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact", "vue" },
-			})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact", "vue" },
+        handlers = {
+          ["textDocument/publishDiagnostics"] = vim.lsp.with(
+            vim.lsp.diagnostic.on_publish_diagnostics,
+            { virtual_text = false, signs = true, update_in_insert = false, underline = true }
+          ),
+        },
+      })
 
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.ruff.setup({
-				capabilities = capabilities,
-				init_options = {
-					settings = {
-						logLevel = "warning",
-					},
-				},
-			})
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
